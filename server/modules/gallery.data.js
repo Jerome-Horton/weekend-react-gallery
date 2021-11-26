b62ae7d4f4a2ab9f420ad5494fafb54b04456a7b
Jerome-Horton <riqui.howard@gmail.com>
2021-11-26 16:01:48 -0600

    modified images id

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -4,37 +4,37 @@ const galleryItems = [
         likes: 0 
     },
 
-    {   id: 1, path: 'images/tacos.jpeg', 
+    {   id: 2, path: 'images/tacos.jpeg', 
         description: 'Photo of an assortment of delicious tacos', 
         likes: 0 
     },
 
-    {   id: 1, path: 'images/santorini_greece.jpeg', 
+    {   id: 3, path: 'images/santorini_greece.jpeg', 
         description: 'Photo of a beautiful resort in Santorini, Greece.', 
         likes: 0 
     },
 
-    {   id: 1, path: 'images/lebronJames.jpg', 
+    {   id: 4, path: 'images/lebronJames.jpg', 
         description: 'Photo of the Great Lebron James.', 
         likes: 0 
     },
 
-    {   id: 1, path: 'images/cristianoRonlado.jpg', 
+    {   id: 5, path: 'images/cristianoRonlado.jpg', 
         description: 'Photo of the Great Cristiano Ronaldo.', 
         likes: 0 
     },
 
-    {   id: 1, path: 'images/crypto-trading.png', 
+    {   id: 6, path: 'images/crypto-trading.png', 
         description: 'Photo of a crypto currency trading market.', 
         likes: 0 
     },
 
-    {   id: 1, path: 'images/geneva-bugatti.jpeg', 
+    {   id: 7, path: 'images/geneva-bugatti.jpeg', 
         description: 'Photo of the most expensive car in the world, Bugatti La Voiture Noire ', 
         likes: 0 
     },
 
-    {   id: 1, path: 'images/beautiful_home.jpeg', 
+    {   id: 8, path: 'images/beautiful_home.jpeg', 
         description: 'Photo of a beautiful home in Beverly Hills.', 
         likes: 0 
     }
