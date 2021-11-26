13d105201b9ffe468ede5337791dc4bad25068b7
Jerome-Horton <riqui.howard@gmail.com>
2021-11-26 15:57:01 -0600

    IAdded Image data - Done

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,41 +1,41 @@
 const galleryItems = [
-    {   id: 1, path: 'images/goat_small.jpg', 
-        description: 'Photo of a goat taken at Glacier National Park.', 
+    {   id: 1, path: 'images/Bob_Marley.jpeg', 
+        description: 'Photo of the Iconic Bob Marley.', 
         likes: 0 
     },
 
-    {   id: 1, path: 'images/goat_small.jpg', 
-        description: 'Photo of a goat taken at Glacier National Park.', 
+    {   id: 1, path: 'images/tacos.jpeg', 
+        description: 'Photo of an assortment of delicious tacos', 
         likes: 0 
     },
 
-    {   id: 1, path: 'images/goat_small.jpg', 
-        description: 'Photo of a goat taken at Glacier National Park.', 
+    {   id: 1, path: 'images/santorini_greece.jpeg', 
+        description: 'Photo of a beautiful resort in Santorini, Greece.', 
         likes: 0 
     },
 
-    {   id: 1, path: 'images/goat_small.jpg', 
-        description: 'Photo of a goat taken at Glacier National Park.', 
+    {   id: 1, path: 'images/lebronJames.jpg', 
+        description: 'Photo of the Great Lebron James.', 
         likes: 0 
     },
 
-    {   id: 1, path: 'images/goat_small.jpg', 
-        description: 'Photo of a goat taken at Glacier National Park.', 
+    {   id: 1, path: 'images/cristianoRonlado.jpg', 
+        description: 'Photo of the Great Cristiano Ronaldo.', 
         likes: 0 
     },
 
-    {   id: 1, path: 'images/goat_small.jpg', 
-        description: 'Photo of a goat taken at Glacier National Park.', 
+    {   id: 1, path: 'images/crypto-trading.png', 
+        description: 'Photo of a crypto currency trading market.', 
         likes: 0 
     },
 
-    {   id: 1, path: 'images/goat_small.jpg', 
-        description: 'Photo of a goat taken at Glacier National Park.', 
+    {   id: 1, path: 'images/geneva-bugatti.jpeg', 
+        description: 'Photo of the most expensive car in the world, Bugatti La Voiture Noire ', 
         likes: 0 
     },
 
-    {   id: 1, path: 'images/goat_small.jpg', 
-        description: 'Photo of a goat taken at Glacier National Park.', 
+    {   id: 1, path: 'images/beautiful_home.jpeg', 
+        description: 'Photo of a beautiful home in Beverly Hills.', 
         likes: 0 
     }
 
