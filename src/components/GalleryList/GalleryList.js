bc2291cb38018fea35e31ee1f4ed90e54f8edc2e
Jerome-Horton <riqui.howard@gmail.com>
2021-11-27 14:26:21 -0600

    Created function updateLikes and Axios PUT route

---
+++ src/components/GalleryList/GalleryList.js
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import GalleryItem from '../GalleryItem/GalleryItem';
+
+// Pass gallery data using props to the GalleryList Component
+// Iterate (loop over) the list of gallery data
+// Make GalleryItems
+
+
+
+export default GalleryList;
