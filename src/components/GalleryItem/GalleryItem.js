ee8ef68839c4a765891253ee176b979b8c76036b
Jerome-Horton <riqui.howard@gmail.com>
2021-11-28 17:27:45 -0600

    created galleryItem funtion & buttons

---
+++ src/components/GalleryItem/GalleryItem.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+// Create Gallery Item function and pass it the individual gallery item via props.
+    function galleryItem({item, updateLikes}) {
+
+        let [showImage, setShowImage] = useState(true);
+
+// UseState is set to true by default, so if description is requested,
+// We need to change setShowImage  to false
+// Create a function for description...do a new component for it to import it.
+// Also new component for images.
+
+
+        if(showImage) {
+        
+            return (
+                <div>
+                    {/* <galleryImages
+                        item = {item}
+                    /> */}
+                    <button onClick={ () => updateLikes(item.id)}>Love It</button>
+                    <p>{item.likes}</p>
+                </div>
+            )
+        }
+
+        return (
+            <div>
+                {/* <galleryDescription
+                    item = {item}
+                /> */}
+                <button onClick={ () => updateLikes(item.id)}>Love It</button>
+                <p>{item.likes}</p>
+            </div>
+        
+        )
+}
+
+
+// Export Gallery Item component to GalleryList component
+export default galleryItem;
